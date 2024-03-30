5653ddd68b59a6eda5fa49cfa634db559f968d35
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-03-30 21:44:34 +0100

    components deleted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,8 @@
-import Product from "./Product";
 
 export default function App() {
   return (
     <div>
       <h1>Products</h1>
-
-      <Product />
-      <Product />
-      <Product />
     </div>
   );
 }
