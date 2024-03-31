b4b58381c80da55677643e5f8b828e974f6ddf48
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-03-31 14:59:41 +0200

    path changed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import Profile from './Profile';
-import userData from '../userData.json';
+import Profile from './profile/Profile';
+import userData from '../components/profile/userData.json';
 
 export default function App() {
   return <Profile data={userData} />;
