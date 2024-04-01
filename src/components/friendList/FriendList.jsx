41052571d0ee5ee32914e6914d58aa39bcb87a36
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-04-01 15:11:56 +0200

    base css fl added

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,13 +1,14 @@
 import FriendListItem from './FriendListItem';
+import css from './FriendList.module.css'
 
 export default function FriendList({ friends }) {
   const list = friends.map(({ id, ...friendInfo }) => {
     return (
-      <li key={id}>
+      <li key={id} className={css.item} >
         <FriendListItem data={friendInfo} />
       </li>
     );
   });
 
-  return <ul>{list}</ul>;
+  return <ul className={css.list}>{list}</ul>;
 }
