ad191d383d51ca8b99a917a0ba54d09e04564c41
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-04-01 16:07:47 +0200

    fl css added

--- src/components/friendList/FriendListItem.jsx
+++ src/components/friendList/FriendListItem.jsx
@@ -1,10 +1,11 @@
+import css from './FriendListItem.module.css'
 export default function FriendListitem({ data: { avatar, name, isOnline } }) {
-  const status = isOnline ? 'Online' : 'Offline';
+  const status = isOnline ? 'online' : 'offline';
   return (
     <div>
       <img src={avatar} alt="Avatar" width="48" />
-      <p>{name}</p>
-      <p>{status}</p>
+      <p className={css.paragraph}>{name}</p>
+      <p className={css[status]}>{status}</p>
     </div>
   );
 }
