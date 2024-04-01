6615c9774fbcbb43df080b645621fa79afe69e31
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-04-01 16:51:08 +0200

    css added

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,3 +1,4 @@
+import css from './TransactionHistory.module.css';
 export default function TransactionHistory({ items }) {
   const rows = items.map(({ id, type, amount, currency }) => {
     return (
