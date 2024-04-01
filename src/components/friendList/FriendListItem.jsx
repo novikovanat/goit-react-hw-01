88ee88f62732cf9bc5999b348ce0b63db1613d58
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-04-01 15:34:21 +0200

    status added

--- src/components/friendList/FriendListItem.jsx
+++ src/components/friendList/FriendListItem.jsx
@@ -1,9 +1,10 @@
 export default function FriendListitem({ data: { avatar, name, isOnline } }) {
+  const status = isOnline ? 'Online' : 'Offline';
   return (
     <div>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
-      <p>{isOnline}</p>
+      <p>{status}</p>
     </div>
   );
 }
